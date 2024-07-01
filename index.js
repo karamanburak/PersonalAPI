3028cfa5d84bade75e7fbf673ebe486295a8a287
Burak Karaman <karaman.buraak@gmail.com>
2024-07-01 09:46:08 +0200

    Update .env-sample, index.js, department.controller.js, and department.model.js files with new configurations, middleware, and schema definitions.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
@@ -6,23 +6,49 @@
     $ npm i express dotenv mongoose express-async-errors
 */
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
+//^ Required Modules
 
+//* envVariables to process.env
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
+//* asyncErrors to errorHandler
+require("express-async-errors");
 
-// continue from here...
+/* -------------------------------------------------------
+                 Configurations
+------------------------------------------------------- */
 
+//! Database connection
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
+/* -------------------------------------------------------
+                 Middlewares
+------------------------------------------------------- */
+//* accept json
+app.use(express.json());
+
+//* Filter,Search, Sort, Pagination (res.getModelList)
+require("./src/middlewares/findSearchSortPagi");
+
+/* -------------------------------------------------------
+                 Routes
+------------------------------------------------------- */
+app.all("/", (req, res) => {
+  res.send("<h1>Welcome to Personal API</h1>");
+});
 
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -1,4 +1,51 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+const Department = require("../models/department.model");
+
+module.exports = {
+  list: async (req, res) => {
+    // const departments = await res.getModelList(Department);
+    const data = await res.getModelList(Department);
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Department),
+      // departments,
+      data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await Department.create(req.body);
+    res.status(201).send({
+      error: false,
+      //   message: "Department created successfully",
+      data,
+    });
+  },
+  read: async (req, res) => {
+    // const data = await Department.findById(req.params.id); //* findById arka planda findOne methodunu calistirir.
+    const data = await Department.findOne({ _id: req.params.id });
+    req.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await Department.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+    req.status(202).send({
+      error: false,
+      data,
+      newData: await Department.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Department.deleteOne({ _id: req.params.id });
+    req.status(data.deletedCount > 0 ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
